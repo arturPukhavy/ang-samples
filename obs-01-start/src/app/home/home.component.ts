fa1404bf69454987456223f5a106cf7855bdfc5d
Eugeny <eburdo@gmail.com>
2024-06-22 18:32:35 +0200

    add observable

---
+++ obs-01-start/src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {interval} from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    //interval(1000).subscribe(count => this.myFun(count))
+    interval(1000).subscribe(count => {
+      console.log(count);
+    })
+  }
+
+  myFun(c: any){
+    console.log(c);
+  }
+
+}
