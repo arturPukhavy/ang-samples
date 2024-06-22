6941706fad99105f4c1a2bf93a26c3537d22658e
Eugeny <eburdo@gmail.com>
2024-06-22 21:59:35 +0200

    minor changes

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {interval} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {Subscription, interval} from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
-    //interval(1000).subscribe(count => this.myFun(count))
+    //Option1 (Use anonimous function)
     interval(1000).subscribe(count => {
       console.log(count);
     })
+
+    //Option2
+    this.firstObsSubscription = interval(1000).subscribe(count => this.logger(count))
   }
 
-  myFun(c: any){
+  logger(c: number){
     console.log(c);
   }
 
+  ngOnDestroy(): void {
+    console.log("Call ngOnDestroy and unsubscribe")
+    this.firstObsSubscription.unsubscribe();
+  }
+ 
 }
